622fbb7f6bd69a7a3463674b0b98b312f39831db
Ben <benbaumann95@hotmail.co.uk>
2020-11-13 12:22:36 +0000

    Render header component from lbh-frontend-react

---
+++ app/javascript/components/Layout/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderComponent from './Header/Header'
+
+const Layout = () => (
+  <div>
+    <HeaderComponent serviceName="Repairs Hub" />
+  </div>
+)
+
+export default Layout
