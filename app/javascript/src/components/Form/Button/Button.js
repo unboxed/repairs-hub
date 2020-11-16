5384a1b6ed585ffa46d24aa4496dab68119e37c7
Ben <benbaumann95@hotmail.co.uk>
2020-11-16 18:02:18 +0000

    Add button component

---
+++ app/javascript/src/components/Form/Button/Button.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = ({ onClick, label, type, ...otherProps }) => (
+  <div className="govuk-form-group">
+    <button
+      className="govuk-button"
+      data-module="govuk-button"
+      onClick={onClick}
+      type={type}
+      {...otherProps}
+    >
+      {label}
+    </button>
+  </div>
+);
+
+export default Button
