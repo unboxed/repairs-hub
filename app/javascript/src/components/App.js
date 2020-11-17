d4c843237917362454850c03db61b1ee3f211846
Ben <benbaumann95@hotmail.co.uk>
2020-11-17 17:57:20 +0000

    Use react-router-dom to separate routes

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Layout from './Layout/index'
 import Search from './Search/Search'
+import PropertiesTable from './Properties/PropertiesTable'
+import PropertyView from './Property/PropertyView'
 
-const App = () => {
-  return (
-    <div>
-      <Layout />
-      <Search />
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Search}/>
+          <Route exact path="/properties" component={PropertiesTable}/>
+          <Route exact path="/properties/:propertyReference" component={PropertyView}/>
+        </Switch>
+      </Layout>
+    )
+  }
 }
 
 export default App
--- app/javascript/src/components/Property/PropertyRow.js
+++ app/javascript/src/components/Property/PropertyRow.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PropertyRow = ({ propertyReference, address, hierarchyType }) => (
   <tr className="govuk-table__row govuk-table__row--clickable">
     <td className="govuk-table__cell">{address.shortAddress}</td>
     <td className="govuk-table__cell">{address.postalCode}</td>
     <td className="govuk-table__cell">{hierarchyType.subTypeDescription}</td>
-    <td className="govuk-table__cell">{propertyReference}</td>
+
+    <td className="govuk-table__cell">
+      <Link to ={`/properties/${propertyReference}`} >{propertyReference}
+      </Link>
+    </td>
+
   </tr>
 )
 
