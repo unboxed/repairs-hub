80aac712ede5c6710165f578a7fa26543e7a88ca
Ben <benbaumann95@hotmail.co.uk>
2020-11-17 17:58:33 +0000

    Add PhaseBanner component to Header

--- app/javascript/src/components/Layout/Header/Header.js
+++ app/javascript/src/components/Layout/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Header } from 'lbh-frontend-react';
+import PhaseBanner from '../PhaseBanner/PhaseBanner';
 
 const HeaderComponent = ({ serviceName }) => (
   <div>
     <Header
-      serviceName={serviceName} isServiceNameShort={false} homepageUrl="/"
-    />
+      serviceName={serviceName} isServiceNameShort={false} homepageUrl="/">
+      <PhaseBanner phase="beta" />
+    </Header>
   </div>
 );
 
--- app/javascript/src/components/Layout/index.js
+++ app/javascript/src/components/Layout/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import HeaderComponent from './Header/Header'
+import PhaseBanner from './PhaseBanner/PhaseBanner';
 
-const Layout = () => (
+const Layout = ({ children }) => (
   <div>
     <HeaderComponent serviceName="Repairs Hub" />
+
+    <div className="govuk-width-container">
+      <main
+        className="govuk-main-wrapper"
+        id="content"
+        role="main"
+      >
+        {children}
+      </main>
+      </div>
   </div>
 )
 
